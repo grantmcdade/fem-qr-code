f0dc860d47bfc969c33ea5d71921baba9eda791d
Grant McDade <grantmcdade@users.noreply.github.com>
2023-03-07 17:45:48 +0100

    Initial files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+  </header>
+
+  <main>
+    <qr-code />
+  </main>
+</template>
+
+<script setup lang="ts">
+import QrCode from '@/components/QrCode.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/QrCode.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+  
+        Improve your front-end skills by building projects
+
+Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
