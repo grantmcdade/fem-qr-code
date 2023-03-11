52e734c57ee95cfd6d35aedd4a1398f98276e86e
Grant McDade <grantmcdade@users.noreply.github.com>
2023-03-11 17:35:14 +0100

    Address report items

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <header>
-  </header>
+    <header></header>
 
-  <main>
-    <qr-code />
-  </main>
+    <main>
+        <h1>Frontend Mentor | QR code component</h1>
+        <div class="container">
+            <qr-code class="center" />
+        </div>
+        <div class="attribution">
+            Challenge by
+            <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+                >Frontend Mentor</a
+            >. Coded by
+            <a href="https://github.com/grantmcdade">Grant McDade</a>.
+        </div>
+    </main>
 </template>
 
 <script setup lang="ts">
-import QrCode from '@/components/QrCode.vue'
+import QrCode from '@/components/QrCode.vue';
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
